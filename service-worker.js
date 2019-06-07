d570895824b3100d276ce1793120a42b4a0c293c
Hanna Diggins <diggins@DESKTOP-A8M6EAL.domain>
2019-06-06 22:36:51 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ms-coding-test/E2/gender-vote/build/precache-manifest.43996046a53c4ba7efc04fa2efb44891.js"
+  "/ms-coding-test/E2/gender-vote/build/precache-manifest.7422e6722d215cb95bcd6103ba2c4005.js"
 );
 
 self.addEventListener('message', (event) => {
