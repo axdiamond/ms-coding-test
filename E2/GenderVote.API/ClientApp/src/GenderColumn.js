275be0b9120f3429db027b0205c1a97dec543852
Andrew Diamond <andrew.diamond1125@gmail.com>
2019-06-10 23:59:03 -0700

    Properly format numbers

--- E2/GenderVote.API/ClientApp/src/GenderColumn.js
+++ E2/GenderVote.API/ClientApp/src/GenderColumn.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import loadingIcon from './img/loading-icon.svg'
 
-const formatAsPercent = (val, demoninator) => {
-  if (demoninator === 0)
-    return '0%';
-
-  return parseFloat((val / demoninator) * 100).toFixed(0) + "%";
-}
-
 function renderLoading() {
   return <img src={loadingIcon} className="gender-voter__loading-icon" alt="loading..." />;
 }
 
 function renderScore(votes, totalVotes) {
+  let ratio = ((votes / totalVotes) * 100);
+  let color = "black";
+
+  if (ratio > 50) {
+    color = "green";
+    ratio = Math.ceil(ratio);
+  } else if (ratio < 50) {
+    ratio = Math.floor(ratio);
+    color = "red";
+  }
+
+  const style = { color: color };
   return (
-    <div className="ui big label">
-      {formatAsPercent(votes, totalVotes)}
+    <div className="ui big label" style={style}>
+      {ratio}%
     </div>
   );
 }
 
-function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, onClick, loading }) {
+function GenderColumn({ className, title, image, votes, totalVotes, onClick, loading }) {
   return (
     <div className={className}>
       <h2 className="ui header">
