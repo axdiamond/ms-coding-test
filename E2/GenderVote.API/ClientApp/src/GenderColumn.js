a566df6d44b21410aa47ad42cea217609a6ff6e7
Andrew Diamond <andrew.diamond1125@gmail.com>
2019-06-10 22:56:11 -0700

    Spacing

--- E2/GenderVote.API/ClientApp/src/GenderColumn.js
+++ E2/GenderVote.API/ClientApp/src/GenderColumn.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 const formatAsPercent = (val, demoninator) => {
-    if (demoninator === 0)
-      return '0%';
+  if (demoninator === 0)
+    return '0%';
 
-    return parseFloat((val / demoninator) * 100).toFixed(0) + "%";
-  }
+  return parseFloat((val / demoninator) * 100).toFixed(0) + "%";
+}
 
-  function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, onClick }) {
-    return (
-      <div className={className}>
-        <h2 className="ui header">
-          {title}
-        </h2>
-        <div>
-          <img src={image} className="gender-voter__image" />
-        </div>
-        <div className="gender-voter__score">
-          <div style={scoreStyle} className="ui big label">
-            {formatAsPercent(votes, totalVotes)}
-          </div>
-        </div>
-        <button className="gender-voter__vote ui massive button green" type="button" onClick={() => onClick()}>
-          Vote!
-        </button>
+function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, onClick }) {
+  return (
+    <div className={className}>
+      <h2 className="ui header">
+        {title}
+      </h2>
+      <div>
+        <img src={image} alt={title} className="gender-voter__image" />
       </div>
-    );
-  }
+      <div className="gender-voter__score">
+        <div style={scoreStyle} className="ui big label">
+          {formatAsPercent(votes, totalVotes)}
+        </div>
+      </div>
+      <button className="gender-voter__vote ui massive button green" type="button" onClick={() => onClick()}>
+        Vote!
+      </button>
+    </div>
+  );
+}
 
-  export default GenderColumn;
+export default GenderColumn;
