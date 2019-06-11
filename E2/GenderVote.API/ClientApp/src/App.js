92a80c236f5b16e4c5c8e80b4c88f9872c3f1b3b
Andrew Diamond <andrew.diamond1125@gmail.com>
2019-06-10 23:37:54 -0700

    use a loading icon

--- E2/GenderVote.API/ClientApp/src/App.js
+++ E2/GenderVote.API/ClientApp/src/App.js
@@ -7,22 +7,25 @@ import femaleImage from './img/female.svg'
 function App() {
   const [femaleVotes, setFemaleVotes] = useState(0);
   const [maleVotes, setMaleVotes] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const totalVotes = maleVotes + femaleVotes;
 
-  const voteMale = () =>
+  function voteMale () {
+    setMaleVotes(maleVotes + 1);
+
     fetch('/api/Votes/VoteMale', {
       method: 'POST'
-    }).then(() => {
-      setMaleVotes(maleVotes + 1);
     });
+  }
 
-  const voteFemale = () =>
+  function voteFemale() {
+    setFemaleVotes(femaleVotes + 1);
+
     fetch('/api/Votes/VoteFemale', {
       method: 'POST'
-    }).then(() => {
-      setFemaleVotes(femaleVotes + 1);
     });
+  }
 
   useEffect(() => {
     fetch('/api/votes')
@@ -30,12 +33,13 @@ function App() {
       .then(r => {
         setMaleVotes(r.maleScore);
         setFemaleVotes(r.femaleScore);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="gender-voter">
-      <div className="ui placeholder segment">
+      <div className="ui segment">
         <div className="ui two column stackable center aligned grid">
           <div className="ui vertical divider">Or</div>
           <div className="middle aligned row">
@@ -45,14 +49,16 @@ function App() {
               image={femaleImage}
               votes={femaleVotes}
               totalVotes={totalVotes}
-              onClick={voteFemale} />
+              onClick={voteFemale}
+              loading={loading} />
             <GenderColumn
               className="ui column"
               title='Male'
               image={maleImage}
               votes={maleVotes}
               totalVotes={totalVotes}
-              onClick={voteMale} />
+              onClick={voteMale}
+              loading={loading} />
           </div>
         </div>
       </div>
--- E2/GenderVote.API/ClientApp/src/GenderColumn.js
+++ E2/GenderVote.API/ClientApp/src/GenderColumn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import loadingIcon from './img/loading-icon.svg'
 
 const formatAsPercent = (val, demoninator) => {
   if (demoninator === 0)
@@ -7,7 +8,19 @@ const formatAsPercent = (val, demoninator) => {
   return parseFloat((val / demoninator) * 100).toFixed(0) + "%";
 }
 
-function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, onClick }) {
+function renderLoading() {
+  return <img src={loadingIcon} className="gender-voter__loading-icon" alt="loading..." />;
+}
+
+function renderScore(votes, totalVotes) {
+  return (
+    <div className="ui big label">
+      {formatAsPercent(votes, totalVotes)}
+    </div>
+  );
+}
+
+function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, onClick, loading }) {
   return (
     <div className={className}>
       <h2 className="ui header">
@@ -17,9 +30,7 @@ function GenderColumn({ className, title, image, scoreStyle, votes, totalVotes, 
         <img src={image} alt={title} className="gender-voter__image" />
       </div>
       <div className="gender-voter__score">
-        <div style={scoreStyle} className="ui big label">
-          {formatAsPercent(votes, totalVotes)}
-        </div>
+        {loading ? renderLoading() : renderScore(votes, totalVotes)}
       </div>
       <button className="gender-voter__vote ui massive button green" type="button" onClick={() => onClick()}>
         Vote!
